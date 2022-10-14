0161d5ce4b9bb15b6efa11778ee359f057a932ed
Jonathan Golan <Jonathanbgolan@gmail.com>
2022-10-14 17:53:23 +0200

    Added delete and put mappings

--- src/main/java/se/knowit/springboot3/controller/BeerController.java
+++ src/main/java/se/knowit/springboot3/controller/BeerController.java
@@ -1,11 +1,9 @@
 package se.knowit.springboot3.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import se.knowit.springboot3.model.Beer;
 import se.knowit.springboot3.repository.BeerScoreRepository;
 
@@ -23,7 +21,29 @@ public class BeerController {
         return beerScoreRepository.findAll();
     }
 
-    @RequestMapping(value = "beers", method = RequestMethod.POST)
+    @GetMapping(value = "beers/{id}")
+    public Beer get(@PathVariable Long id) {
+        return beerScoreRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping(value = "beers/{id}")
+    public Beer delete(@PathVariable Long id) {
+        Beer deletedBeer = beerScoreRepository.findById(id).orElse(null);
+        if (deletedBeer == null) {
+            return null;
+        }
+        beerScoreRepository.delete(deletedBeer);
+        return deletedBeer;
+    }
+
+    @PutMapping(value = "beers/{id}")
+    public Beer update(@PathVariable Long id, @RequestBody Beer beer) {
+        Beer existingBeer = beerScoreRepository.findById(id).orElse(null);
+        assert existingBeer != null;
+        return beerScoreRepository.saveAndFlush(beer);
+    }
+
+    @PostMapping(value = "beers")
     public Beer postBeer(@RequestBody Beer beer) {
         return beerScoreRepository.saveAndFlush(beer);
     }
