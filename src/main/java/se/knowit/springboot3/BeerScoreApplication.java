aa9addc8613590b055ee27911f845cd80ae15573
Morgan Berg <morgan.berg@knowit.se>
2022-10-14 16:22:10 +0200

    Configure H2 db and initialised with sample beer data

--- src/main/java/se/knowit/springboot3/BeerScoreApplication.java
+++ src/main/java/se/knowit/springboot3/BeerScoreApplication.java
@@ -9,5 +9,4 @@ public class BeerScoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BeerScoreApplication.class, args);
 	}
-
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO beer (id, name, description, score) VALUES (1, 'Mikkeller', 'Ljus lager', 10);
+INSERT INTO beer (id, name, description, score) VALUES (2, 'Paulaner', 'Ljus lager', 8);
+INSERT INTO beer (id, name, description, score) VALUES (3, 'Newcastle', 'Ale', 7);
